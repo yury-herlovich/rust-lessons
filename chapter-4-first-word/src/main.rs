c5de1c65b74b0c437900e980865b483375fcf3e6
Yury Herlovich <yuryherlovich@gmail.com>
2021-06-26 14:37:44 -0700

    4: first word

---
+++ chapter-4-first-word/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let s = String::from("Hello, world!");
+
+    println!("First word is {}", first_word(&s))
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
+}
