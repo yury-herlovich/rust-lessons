be101378718249de554b45662719894e749cf625
Yury Herlovich <yuryherlovich@gmail.com>
2021-07-28 19:54:33 -0700

    chapter 6 - enums, matching

---
+++ chapter-6/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let _x = plus_one(Some(1));
+    let _none = plus_one(None);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
